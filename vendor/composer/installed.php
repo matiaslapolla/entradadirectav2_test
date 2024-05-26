82aa39e8fa230d6999297beb6dbf31a9b828b3d6
Matias Lapolla <matias.lapolla.1@gmail.com>
2024-05-26 19:54:38 -0300

    modify the composer installed

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '61e144556f5637cb4b9d9203709552c6addb0892',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '61e144556f5637cb4b9d9203709552c6addb0892',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
